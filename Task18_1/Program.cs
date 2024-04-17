620962cddf337dab95874f618700e7ff8735ed5e
Svetlana Jourkova <belkina_s@mail.ru>
2024-04-17 22:59:21 +0300

    Добавьте файлы проекта.

---
+++ Task18_1/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Task18_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку");
+            string str = Console.ReadLine();
+            Console.WriteLine(Check(str));
+        }
+
+        static bool Check(string str)
+        {
+            Stack<char> stack = new Stack<char>();
+            Dictionary<char, char> sk = new Dictionary<char, char>()
+            {
+                { '(',')'},
+                { '{','}'},
+                { '[',']'},
+            };
+
+            foreach (char c in str)
+            {
+                if (c == '(' || c == '{' || c == '[')
+                    stack.Push(sk[c]);
+                if (c == ')' || c == '}' || c == ']')
+                {
+                    if (stack.Count == 0 || stack.Pop() != c)
+                    {
+                        return false;
+                    }
+                }
+            }
+            if (stack.Count == 0)
+                return true;
+            else 
+                return false;
+        }
+    }
+}
